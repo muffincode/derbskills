6092daed02ea36eda3ad8441af79f2fa21ad9f83
Camille Fite <camille.fite@gmail.com>
2025-02-08 19:13:19 +0100

    feat(tags,authors): add new features
    - Tags for filtering
    - Author for ... ego boost? And planning for contributions

--- content.config.ts
+++ content.config.ts
@@ -2,23 +2,39 @@ import { defineContentConfig, defineCollection, z } from '@nuxt/content'
 
 export default defineContentConfig({
   collections: {
-    content: defineCollection({
-      type: 'page',
-      source: '**/*.md'
+    tags: defineCollection({
+      source: '2.tags/**.yml',
+      type: 'data',
+      schema: z.object({
+        key: z.string(),
+        fr: z.string(),
+        icon: z.string()
+      })
+    }),
+    authors: defineCollection({
+      source: '3.authors/**.yml',
+      type: 'data',
+      schema: z.object({
+        key: z.string(),
+        name: z.string(),
+        club: z.string()
+      })
     }),
     questions: defineCollection({
-        source: '**/*.yml',
-        type: 'data',
-        schema: z.object({
-            label: z.string(),
-            hint: z.string(),
-            possibleAnswers: z.array(z.string()),
-            answer: z.string(),
-            explanation: z.string(),
-            tags: z.array(z.string()),
-            difficulty: z.string(),
-            aiGenerated: z.boolean()
-        })
+      source: '1.questions/**.yml',
+      type: 'data',
+      schema: z.object({
+        label: z.string(),
+        hint: z.string(),
+        possibleAnswers: z.array(z.string()),
+        answer: z.string(),
+        explanation: z.string(),
+        tags: z.array(z.string()),
+        difficulty: z.string(),
+        aiGenerated: z.boolean(),
+        reviewer: z.string(),
+        author: z.string()
+      })
     })
   }
 })
