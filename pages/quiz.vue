79de326aeedf87a6b309c9034fb1480e4383272a
Camille Fite <camille.fite@gmail.com>
2025-02-09 19:30:19 +0100

    feat(quiz): recap + highlight errors

--- pages/quiz.vue
+++ pages/quiz.vue
@@ -4,7 +4,9 @@
       <main class="bg-cute p-6 relative">
         <header class="flex justify-between">
           <h2 class="text-white">{{ qn + 1 }} / {{ n }}</h2>
-          <h2 class="text-white font-thin"><NuxtLink to="/">quitter le quiz</NuxtLink></h2>
+          <h2 class="text-white font-thin">
+            <NuxtLink to="/">quitter le quiz</NuxtLink>
+          </h2>
         </header>
 
         <p class="text-3xl text-black mt-10 text-balance">{{ question.label }}</p>
@@ -20,11 +22,11 @@
         </details>
       </main>
       <footer class="bg-white p-6 grid gap-3 h-max md:w-1/2 md:mx-auto">
-        <template v-if="state">
-          <div v-if="state === 'success'" class="bg-green-600 text-white rounded p-2 mt-5">
+        <template v-if="state != null">
+          <div v-if="state === true" class="bg-green-600 text-white rounded p-2 mt-5">
             <p>🥳  Bien joué</p>
           </div>
-          <div v-if="state === 'failure'" class="bg-red-600 text-white rounded p-2 mt-5">
+          <div v-if="state === false" class="bg-red-600 text-white rounded p-2 mt-5">
             <p>😖  Eh, c'est pas ça...</p>
           </div>
           <p class="italic text-gray-600">{{ question.explanation }}</p>
@@ -34,7 +36,7 @@
           </button>
         </template>
         <template v-else>
-          <button v-for="a in question.possibleAnswers" class="btn text-white" @click="compute(a)" :disabled="state != ''"
+          <button v-for="a in question.possibleAnswers" class="btn text-white" @click="compute(a)"
             :class="{ 'btn-outline': myAnswer === a }">
             {{ cap(a) }}
           </button>
@@ -47,6 +49,17 @@
 
         <p class="text-3xl text-black mt-10 text-balance">Terminé!</p>
         <p class="text-9xl text-white">{{ score }}<small class="text-2xl">/ {{ n }}</small></p>
+
+        <div class="grid gap-3 mt-6">
+          <article v-for="(q, i) in data" :key="q.id" class="bg-white text-black rounded p-3 border border-solid"
+            :class="{
+              'border-red-600 !bg-red-100': !goodAnswers[i]
+            }">
+            <p>{{ q.label }}</p>
+            <p class="font-bold">{{ q.answer }}</p>
+            <p class="text-sm text-gray-500">{{ q.explanation }}</p>
+          </article>
+        </div>
       </main>
       <footer class="bg-white p-6 grid gap-3 h-max">
         <NuxtLink class="btn btn-outline btn-info" to="/">
@@ -74,7 +87,6 @@ const db = await queryCollection('questions')
 // TODO: Filtering should happen with Nuxt Content but filter in arrays don't work yet
 let data = []
 if (tags) {
-  db.forEach(q => console.log(q.tags?.includes(tags)))
   data = db.filter(q => q.tags?.includes(tags))
 }
 if (type) {
@@ -96,7 +108,8 @@ const qn = ref(0);
 const score = ref(0);
 
 // Questions
-const state = ref("");
+const goodAnswers = ref([undefined]);
+const state = computed(() => goodAnswers.value[qn.value]);
 const myAnswer = ref("");
 
 const question = computed(() => {
@@ -106,15 +119,15 @@ const question = computed(() => {
 const compute = (answer) => {
   myAnswer.value = answer;
   if (answer.toLowerCase() === question.value.answer.toLowerCase()) {
-    state.value = "success";
+    goodAnswers.value[qn.value] = true;
     score.value++
   } else {
-    state.value = "failure";
+    goodAnswers.value[qn.value] = false;
   }
 };
 const next = () => {
-  qn.value++;
-  state.value = "";
-  myAnswer.value = "";
+  qn.value++
+  goodAnswers.value.push(undefined)
+  myAnswer.value = ""
 };
 </script>
